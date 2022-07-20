be812e38b972a91ae9ece286c108297fd19918a0
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-20 12:00:48 +0430

    add Response

---
+++ src/main/java/shared/response/Response.java
@@ -0,0 +1,36 @@
+package shared.response;
+
+import java.util.HashMap;
+
+public class Response {
+    private ResponseStatus status;
+    private HashMap<String, Object> data;
+    private String errorMessage;
+
+    public Response() {}
+
+    public Response(ResponseStatus status) {
+        this.status = status;
+        this.data = new HashMap<>();
+    }
+
+    public ResponseStatus getStatus() {
+        return this.status;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public Object getData(String dataName) {
+        return this.data.get(dataName);
+    }
+
+    public void addData(String dataName, Object data) {
+        this.data.put(dataName, data);
+    }
+}
