b657101cadd8be60072c8cc69ad3287a10fd9b01
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-22 13:32:41 +0430

    make serverController static

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -4,7 +4,7 @@ import client.network.ServerController;
 
 public class Client {
     private final int port;
-    private ServerController serverController;
+    public static ServerController serverController;
 
     public Client(int port) {
         this.port = port;
@@ -12,7 +12,7 @@ public class Client {
 
     //TODO : START GUI
     protected void start() {
-        this.serverController = new ServerController(this.port);
+        serverController = new ServerController(this.port);
         serverController.connectToServer();
     }
 }
