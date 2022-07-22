06a7420420ef4cd6073341c4e83b7de619888324
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-22 11:28:20 +0430

    change captcha fields

--- src/main/java/server/logic/captcha/Captcha.java
+++ src/main/java/server/logic/captcha/Captcha.java
@@ -1,14 +1,12 @@
 package server.logic.captcha;
 
-import javafx.scene.image.Image;
-
 public class Captcha {
-    private Image captchaImage;
+    private String captchaImageAddress;
     private String captchaValue;
     private int captchaID;
 
-    public Captcha(Image captchaImage, String captchaValue) {
-        this.captchaImage = captchaImage;
+    public Captcha(String captchaImageAddress, String captchaValue) {
+        this.captchaImageAddress = captchaImageAddress;
         this.captchaValue = captchaValue;
     }
 
