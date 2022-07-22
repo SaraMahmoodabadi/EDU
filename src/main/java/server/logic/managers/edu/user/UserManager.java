30580f078b74945cda2a10757f87025f94e3830d
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-22 13:33:45 +0430

    send captcha image

--- src/main/java/server/logic/managers/edu/user/UserManager.java
+++ src/main/java/server/logic/managers/edu/user/UserManager.java
@@ -1,6 +1,14 @@
 package server.logic.managers.edu.user;
 
+import shared.response.Response;
+
 public class UserManager {
+
+    public Response sendCaptchaImage() {
+        int captchaID = 0;
+        String query = "SELECT * FROM captcha WHERE captchaID = " + captchaID;
+        return null;
+    }
 
     //TODO
     public boolean changePassword() {
