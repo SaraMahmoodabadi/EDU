2a91076ee5eb67b101a9074ba75e5bea18c724da
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-22 13:33:02 +0430

    add getImage method

--- src/main/java/shared/util/media/ImageHandler.java
+++ src/main/java/shared/util/media/ImageHandler.java
@@ -1,5 +1,13 @@
 package shared.util.media;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -27,5 +35,33 @@ public class ImageHandler {
             return Base64.getDecoder().decode(encoded);
         }
         return null;
+    }
+
+    public Image getImage(String encoded) {
+        byte[] data = this.decode(encoded);
+        if (data != null) {
+            ByteArrayInputStream bis = new ByteArrayInputStream(data);
+            try {
+                BufferedImage image = ImageIO.read(bis);
+                return convertToFxImage(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private Image convertToFxImage(BufferedImage image) {
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+        return new ImageView(wr).getImage();
     }
 }
