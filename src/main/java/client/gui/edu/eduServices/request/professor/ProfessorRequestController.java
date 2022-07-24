1a446d96dc9e815a1c2e7024f3644e459273de21
M-Sara <smahmoodabadi2002@gmail.com>
2022-07-24 10:35:46 +0430

    handle professor request page

--- src/main/java/client/gui/edu/eduServices/request/professor/ProfessorRequestController.java
+++ src/main/java/client/gui/edu/eduServices/request/professor/ProfessorRequestController.java
@@ -1,13 +1,26 @@
 package client.gui.edu.eduServices.request.professor;
 
+import client.gui.AlertMonitor;
+import client.gui.EDU;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
+import shared.model.user.professor.Type;
+import shared.request.Request;
+import shared.request.RequestType;
+import shared.response.Response;
+import shared.response.ResponseStatus;
+import shared.util.config.Config;
+import shared.util.config.ConfigType;
 
-public class ProfessorRequestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProfessorRequestController implements Initializable {
 
     @FXML
     protected AnchorPane pane;
@@ -47,13 +60,80 @@ public class ProfessorRequestController {
     protected Button back;
     @FXML
     protected ImageView backImage;
+    private ToggleGroup result;
 
     public void registerRecommendation(ActionEvent actionEvent) {
+        if (isNullRecommendation()) showNullAlert("nullItem");
+        else {
+            Request request = new Request(RequestType.REGISTER_RECOMMENDATION);
+            request.addData("studentCode", studentCode.getText());
+            request.addData("firstBlank", passedLessonsField.getText());
+            request.addData("secondBlank", scoresField.getText());
+            request.addData("thirdBlank", assistantField.getText());
+            showRequestResult(request);
+        }
     }
 
     public void register(ActionEvent actionEvent) {
+        if (isNullRequest()) showNullAlert("nullError");
+        else {
+            Request request = new Request(RequestType.REGISTER_REQUEST_ANSWER);
+            request.addData("studentCode", studentCode.getText());
+            request.addData("type", requestBox.getValue());
+            boolean requestResult = result.getSelectedToggle() == accept;
+            request.addData("result", requestResult);
+            showRequestResult(request);
+        }
     }
 
     public void back(ActionEvent actionEvent) {
+        EDU.sceneSwitcher.switchScene(actionEvent, "mainPage");
+    }
+
+    private boolean isNullRecommendation() {
+        if (studentCode.getText() == null) return true;
+        return passedLessonsField.getText() == null &&
+                scoresField.getText() == null &&
+                assistantField.getText() == null;
+    }
+
+    private boolean isNullRequest() {
+        return studentCode.getText() == null ||
+                requestBox.getValue() == null ||
+                result.getSelectedToggle() == null;
+    }
+
+    private void showRequestResult(Request request) {
+        Response response = EDU.serverController.sendRequest(request);
+        if (response.getStatus() == ResponseStatus.ERROR) {
+            AlertMonitor.showAlert(Alert.AlertType.ERROR, response.getErrorMessage());
+        }
+        else {
+            AlertMonitor.showAlert(Alert.AlertType.INFORMATION, response.getNotificationMessage());
+        }
+    }
+
+    private void showNullAlert(String errorName) {
+        String message = Config.getConfig(ConfigType.GUI_TEXT).getProperty(errorName);
+        AlertMonitor.showAlert(Alert.AlertType.ERROR, message);
+    }
+
+    private void makeBox() {
+        if (EDU.professorType == Type.EDUCATIONAL_ASSISTANT) {
+            requestBox.getItems().add(shared.model.message.request.Type.WITHDRAWAL.toString());
+            requestBox.getItems().add(shared.model.message.request.Type.MINOR.toString());
+        }
+    }
+
+    private void setGroup() {
+        result = new ToggleGroup();
+        accept.setToggleGroup(result);
+        reject.setToggleGroup(result);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        makeBox();
+        setGroup();
     }
 }
